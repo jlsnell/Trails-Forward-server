7c1fd560bc242dc242c96770785950141b0afff3
Ben Shapiro <Ben@getdown.org>
2011-04-18 09:48:59 -0500

    added limit of 200 resource tiles to a megatile index request

--- app/controllers/megatiles_controller.rb
+++ app/controllers/megatiles_controller.rb
@@ -8,6 +8,9 @@ class MegatilesController < ApplicationController
       @megatiles = Megatile.where(:world_id => @world.id).where("x >= :x_min AND x<= :x_max AND y>=:y_min AND y<=:y_max", 
        {:x_min => params[:x_min], :x_max => params[:x_max], :y_min => params[:y_min], :y_max => params[:y_max]})
     else
+      if @world.width * @world.height > 200
+        render :status => :forbidden
+      end
       @megatiles = @world.megatiles
     end
     respond_to do |format|
