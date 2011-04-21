9910976e285efbb7dd812683d8f1a6d58386c3d0
Ben Shapiro <Ben@getdown.org>
2011-04-20 21:03:43 -0500

    make the validation a little richer

--- app/models/water_tile.rb
+++ app/models/water_tile.rb
@@ -2,9 +2,9 @@ class WaterTile < ResourceTile
   validate :should_not_have_land_properties
   
   def should_not_have_land_properties
-    errors.add(:people_density, "illegal for water tiles") if people_density
-    errors.add(:housing_density, "illegal for water tiles") if housing_density
-    errors.add(:development_intensity, "illegal for water tiles") if development_intensity
+    errors.add(:people_density, "illegal for water tiles") unless [nil,0,0.0].include? people_density
+    errors.add(:housing_density, "illegal for water tiles") unless [nil,0,0.0].include? housing_density 
+    errors.add(:development_intensity, "illegal for water tiles") unless [nil,0,0.0].include? development_intensity
   end 
 
   #there doesn't seem to be a way to have extension follow inheritance
