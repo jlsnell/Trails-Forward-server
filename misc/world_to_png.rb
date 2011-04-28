d6215b53aac9d8fd26c46ecc25368ffe3f059ac3
Ben Shapiro <github@catchall.getdown.org>
2011-04-28 15:23:38 -0500

    do one huge db query up front

--- misc/world_to_png.rb
+++ misc/world_to_png.rb
@@ -3,17 +3,20 @@ require 'chunky_png'
 world = World.find ARGV[0]
 canvas = ChunkyPNG::Image.new world.width, world.height, ChunkyPNG::Color::TRANSPARENT
 
-world.width.times do |x|
-  world.height.times do |y|
-    rt = world.resource_tile_at x,y
-    case rt.type
-      when WaterTile.to_s
-        canvas[x,y] = ChunkyPNG::Color :blue
-      when LandTile.to_s
-        color = ChunkyPNG::Color(:green, rt.tree_density)
-        canvas[x,y] = color 
-    end
+# world.width.times do |x|
+#   world.height.times do |y|
+#     rt = world.resource_tile_at x,y
+world.resource_tiles.each do |rt|
+  x = rt.x
+  y = rt.y
+  case rt.type
+    when WaterTile.to_s
+      canvas[x,y] = ChunkyPNG::Color :blue
+    when LandTile.to_s
+      color = ChunkyPNG::Color(:green, rt.tree_density)
+      canvas[x,y] = color 
   end
+  print "."
 end
 
 canvas.save ARGV[1]
